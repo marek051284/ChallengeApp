7a56a1e9d2858c3ba1d0e4f81cdac522d1729f6a
Marek <greda.marek@gmail.com>
2023-10-02 21:20:03 +0200

    Zadanie - dzien 4

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string Name = "Ewa";
+char Gender = 'k';
+int Age = 34;
+
+if(Age > 30 && Name == "Ewa")
+
+ {if (Gender == 'k')
+    
+    {Console.WriteLine(Name + ", lat " + Age); }
+    
+    else {Console.WriteLine("Niepelnoletni mezczyzna");}
+ }
+
+else { Console.WriteLine("Kobieta ponizej 30 lat"); }
+
